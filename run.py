4f4782ed36d57b3bf94bb1d7983922366bf9b246
jakelashbrook <jakeashleylashbrook@gmail.com>
2021-07-27 13:16:24 +0000

    Add Validation to check correct amount of values input

--- run.py
+++ run.py
@@ -24,7 +24,28 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60")
 
     data_str = input("Enter your data here:")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted to int, or 
+    if there are not exactly six values (6 items being sold,
+    so 6 sales data to be recorded.)
+    """
+    try:
+        # if length of input list is more than 6
+        if len(values) != 6:
+            # raise error
+            raise ValueError(
+            # format string response with amount of values input
+            f"Exactly 6 values required, you provided {len(values)}"
+        )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.")
 
 
 get_sales_data()
